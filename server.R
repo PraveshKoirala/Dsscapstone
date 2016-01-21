285bb5651295511245fbb67a9eec4fa51083124c
praveshkoirala@gmail.com <praveshkoirala@gmail.com>
2016-01-21 16:43:06 +0545

    First deploy to shiny server

--- server.R
+++ server.R
@@ -16,7 +16,6 @@ shinyServer(function(input, output) {
   
   # Fill in the spot we created for a plot
   vals <- reactive({getPredictions(input$phrase)})
-  print(vals)
   output$nextWord1 <- renderText(vals()[1])
   output$nextWord2 <- renderText(vals()[2])
   output$nextWord3 <- renderText(vals()[3])
--- ui.R
+++ ui.R
@@ -5,18 +5,24 @@ library(shinythemes)
 shinyUI(
   
   # Use a fluid Bootstrap layout
-  fluidPage(theme=shinytheme("cosmo"),    
-    
+  fluidPage(theme=shinytheme("cosmo"),
+            
+            tags$head(
+              tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+            ),
+            
     # Give the page a title
-    titlePanel("Next word prediction"),
+    h1("Next word prediction", align="center", 
+       style="background-color: grey; color: white; padding-bottom: 5px; padding-top: 5px"),
     
     # Generate a row with a sidebar
-    sidebarLayout(      
+    sidebarLayout(
       
       # Define the sidebar with one input
-      sidebarPanel(
+      sidebarPanel( width=5,
         tags$textarea(id="phrase", label="Phrase:", 
-                      placeholder = "Enter your phrase here", rows=5, cols=33),
+                      placeholder = "Enter your phrase here", style="width:100%; height:100px" 
+                      ),
         hr(),
         helpText("This product uses Kneser-Ney interpolation to predict next word. Just 
                  enter a phrase and possible next words will be given.")
@@ -25,8 +31,8 @@ shinyUI(
       ),
       
       # Create a spot for the barplot
-      mainPanel(
-        h3("Possible next words:"),
+      mainPanel( width=5,
+        h3("Possible next words:", style="font-weight: bold; color: #555"),
         h1(textOutput("nextWord1")),
         h2(textOutput("nextWord2")),
         h3(textOutput("nextWord3"))
